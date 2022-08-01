1d79e8bdd92310ed1f6c17620d4ec6db6cb54fc6
Eugen Soloviov <suenot@gmail.com>
2022-08-01 20:42:15 +0000

    try to getState and try to run pupeeter

--- payments.cjs
+++ payments.cjs
@@ -1,8 +1,11 @@
 require('react');
 require('graphql');
-require('lodash');
+const _ = require('lodash');
 require('subscriptions-transport-ws');
 require('dotenv').config();
+const crypto = require('crypto');
+const axios = require('axios');
+const puppeteer = require('puppeteer');
 
 const { generateApolloClient } = require("@deep-foundation/hasura/client");
 const { DeepClient } = require('@deep-foundation/deeplinks/imports/client');
@@ -18,6 +21,190 @@ const apolloClient = generateApolloClient({
 const unloginedDeep = new DeepClient({ apolloClient });
 
 const delay = (time = 1000) => new Promise(res => setTimeout(res, time));
+
+const tokenize = (options) => {
+  return {
+    ...options,
+    Token: generateToken(options),
+  };
+};
+
+const payInBrowser = async ({ page, browser, url }) => {
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await sleep(3000);
+  const oldForm = await page.evaluate(() => {
+    return !!document.querySelector('input[automation-id="tui-input-card-grouped__card"]');
+  });
+  if (oldForm) {
+    console.log('OLD FORM!!!!!!!');
+    // Старая форма используется на тестовом сервере
+    const cvc1 = await page.evaluate(() => {
+      return !!document.querySelector('button[automation-id="pay-card__submit"]');
+    });
+    if (cvc1) {
+      await page.waitFor('input[automation-id="tui-input-card-grouped__card"]');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__card"]', process.env.PAYMENT_TEST_CARD_NUMBER_SUCCESS); // card number
+      await sleep(300);
+      await page.keyboard.press('Tab');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__expire"]', process.env.PAYMENT_TEST_CARD_EXPDATE); // expired date
+      await sleep(300);
+      await page.keyboard.press('Tab');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__cvc"]', process.env.PAYMENT_TEST_CARD_CVC); // CVC code
+      await sleep(300);
+      await page.click('button[automation-id="pay-card__submit"]'); // submit button
+    } else {
+      await page.waitFor('input[automation-id="tui-input-card-grouped__card"]');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__card"]', process.env.PAYMENT_E2C_CARD_NUMBER_SUCCESS); // card number
+      await sleep(300);
+      await page.keyboard.press('Tab');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__expire"]', process.env.PAYMENT_E2C_CARD_EXPDATE); // expired date
+      await sleep(300);
+      await page.keyboard.press('Tab');
+      await sleep(300);
+      await page.type('input[automation-id="tui-input-card-grouped__cvc"]', process.env.PAYMENT_E2C_CARD_CVC); // CVC code
+      await sleep(300);
+      await page.click('button[automation-id="pay-wallet__submit"]'); // submit button
+      await sleep(300);
+      await page.waitFor('input[name="password"]');
+      const code = prompt('enter code ');
+      console.log('code', code);
+      await page.type('input[name="password"]', code);
+      await sleep(1000);
+    }
+    // TODO: пока старая форма вызывалась только на тестовой карте, где ввод смс кода не нужен
+    await sleep(1000);
+  } else {
+    console.log('NEW FORM!!!!!!!');
+    await page.type('#pan', process.env.PAYMENT_E2C_CARD_NUMBER_SUCCESS); // card number
+    await page.type('#expDate', process.env.PAYMENT_E2C_CARD_EXPDATE); // expired date
+    await page.type('#card_cvc', process.env.PAYMENT_E2C_CARD_CVC); // CVC code
+    await page.click('button[type=submit]'); // submit button
+    await page.waitFor('input[name="password"]');
+    const code = prompt('enter code ');
+    console.log('code', code);
+    await page.type('input[name="password"]', code);
+    await sleep(3000);
+  }
+  await browser.close();
+};
+
+// TODO: дублирование с кодом хендлера
+const errorsConverter = {
+  7:  'Покупатель не найден',
+  53: 'Обратитесь к продавцу',
+  99: 'Платеж отклонен',
+  100:  'Повторите попытку позже',
+  101:  'Не пройдена идентификация 3DS',
+  102:  'Операция отклонена, пожалуйста обратитесь в интернет-магазин или воспользуйтесь другой картой',
+  103:  'Повторите попытку позже',
+  119:  'Превышено кол-во запросов на авторизацию',
+  191:  'Некорректный статус договора, обратитесь к вашему менеджеру',
+  1001: 'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+  1003: 'Неверный merchant ID',
+  1004: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+  1005: 'Платеж отклонен банком, выпустившим карту',
+  1006: 'Свяжитесь с банком, выпустившим карту, чтобы провести платеж',
+  1007: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+  1008: 'Платеж отклонен, необходима идентификация',
+  1012: 'Такие операции запрещены для этой карты',
+  1013: 'Повторите попытку позже',
+  1014: 'Карта недействительна. Свяжитесь с банком, выпустившим карту',
+  1015: 'Попробуйте снова или свяжитесь с банком, выпустившим карту',
+  1019: 'Платеж отклонен — попробуйте снова',
+  1030: 'Повторите попытку позже',
+  1033: 'Истек срок действия карты. Свяжитесь с банком, выпустившим карту',
+  1034: 'Попробуйте повторить попытку позже',
+  1038: 'Превышено количество попыток ввода ПИН-кода',
+  1039: 'Платеж отклонен — счет не найден',
+  1041: 'Карта утеряна. Свяжитесь с банком, выпустившим карту',
+  1043: 'Карта украдена. Свяжитесь с банком, выпустившим карту',
+  1051: 'Недостаточно средств на карте',
+  1053: 'Платеж отклонен — счет не найден',
+  1054: 'Истек срок действия карты',
+  1055: 'Неверный ПИН',
+  1057: 'Такие операции запрещены для этой карты',
+  1058: 'Такие операции запрещены для этой карты',
+  1059: 'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+  1061: 'Превышен дневной лимит платежей по карте',
+  1062: 'Платежи по карте ограничены',
+  1063: 'Операции по карте ограничены',
+  1064: 'Проверьте сумму',
+  1065: 'Превышен дневной лимит транзакций',
+  1075: 'Превышено число попыток ввода ПИН-кода',
+  1076: 'Платеж отклонен — попробуйте снова',
+  1077: 'Коды не совпадают — попробуйте снова',
+  1080: 'Неверный срок действия',
+  1082: 'Неверный CVV',
+  1086: 'Платеж отклонен — не получилось подтвердить ПИН-код',
+  1088: 'Ошибка шифрования. Попробуйте снова',
+  1089: 'Попробуйте повторить попытку позже',
+  1091: 'Банк, выпустивший карту недоступен для проведения авторизации',
+  1092: 'Платеж отклонен — попробуйте снова',
+  1093: 'Подозрение в мошенничестве. Свяжитесь с банком, выпустившим карту',
+  1094: 'Системная ошибка',
+  1096: 'Повторите попытку позже',
+  9999: 'Внутренняя ошибка системы',
+};
+
+const getError = errorCode => errorCode === '0' ? undefined : (errorsConverter[errorCode] || 'broken');
+
+const getUrl = method => `${process.env.PAYMENT_EACQ_AND_TEST_URL}/${method}`;
+
+const _generateToken = (dataWithPassword) => {
+  const dataString = Object.keys(dataWithPassword)
+    .sort((a, b) => a.localeCompare(b))
+    .map(key => dataWithPassword[key])
+    .reduce((acc, item) => 'acc+item', '');
+  const hash = crypto
+    .createHash('sha256')
+    .update(dataString)
+    .digest('hex');
+  return hash;
+};
+
+const generateToken = (data) => {
+  const { Receipt, DATA, Shops, ...restData } = data;
+  const dataWithPassword = { ...restData, Password: process.env.PAYMENT_EACQ_TERMINAL_PASSWORD };
+  return _generateToken(dataWithPassword);
+};
+
+
+const getStateEACQ = async (options) => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: getUrl('GetState'),
+      data: options,
+    });
+
+    const error = getError(response.data.ErrorCode);
+
+    const d = {
+      error,
+      request: options,
+      response: response.data,
+    };
+    console.log(d);
+    options?.log && options.log(d);
+
+    return {
+      error,
+      request: options,
+      response: response.data,
+    };
+  } catch (error) {
+    return {
+      error,
+      request: options,
+      response: null,
+    };
+  }
+};
 
 const f = async () => {
   const guest = await unloginedDeep.guest();
@@ -241,17 +428,17 @@ const f = async () => {
         to_id: PPay
       },
       {
-        type_id: TreeIncludeUp, // TreeIncludeUp
+        type_id: TreeIncludeDown, // TreeIncludeUp
         to_id: PUrl
       },
-      {
-        type_id: TreeIncludeUp,
-        to_id: PPayed
-      },
-      {
-        type_id: TreeIncludeUp,
-        to_id: PError
-      },
+      // {
+      //   type_id: TreeIncludeUp,
+      //   to_id: PPayed
+      // },
+      // {
+      //   type_id: TreeIncludeUp,
+      //   to_id: PError
+      // },
     ] },
   });
 
@@ -453,14 +640,13 @@ const f = async () => {
     console.log({ initData: initData });
 
     const initResult = await initEACQ(initData);
-
-    console.log({ initResult: initResult });
+    console.log({ initResult });
     
     const { data: [{ id: url1 }] } = await deep.insert({
       type_id: await deep.id('@deep-foundation/payments', 'Url'),
       from_id: tinkoffProvider1,
       to_id: newLink.id,
-      json: { data: { value: initResult?.response } }, // теперь пишу весь ответ из тинькова, чтобы обрабатывать и ошибки
+      object: { data: { value: initResult?.response } }, // теперь пишу весь ответ из тинькова, чтобы обрабатывать и ошибки
     });
 
     console.log({ url1: url1 });
@@ -563,7 +749,7 @@ const f = async () => {
 
   console.log({ mp3: mp3 });
 
-  await delay(10000);
+  await delay(20000);
 
   // up
   const { data: mp4 }  = await deep.select({
@@ -585,23 +771,36 @@ const f = async () => {
   })
   console.log({ mp6: mp6 });
 
-  // TODO: получить id Url
+  const url1 = _.find(mp4, { type_id: await deep.id('@deep-foundation/payments', 'Url') });
+  console.log({ url1 });
 
-  // await payInBrowser({
-  //   browser,
-  //   page,
-  //   url: initResult.response.PaymentURL,
-  // });
+  const PaymentId = url1?.value?.value?.PaymentId;
+  console.log({ PaymentId });
 
-  // const newConfirmData = {
-  //   TerminalKey: process.env.PAYMENT_EACQ_TERMINAL_KEY,
-  //   PaymentId: Number(initResult.response.PaymentId),
-  //   Shops: [{ ShopCode: 481488, Amount: 3000, Fee: 210 }],
-  // };
+  const PaymentURL = url1?.value?.value?.PaymentURL;
+  console.log({ PaymentURL });
 
-  // const getStateEACQResult = await getStateEACQ(tokenize(newConfirmData));
-  // console.log('getStateEACQResult', getStateEACQResult);
+  const newConfirmData = {
+    TerminalKey: process.env.PAYMENT_EACQ_TERMINAL_KEY,
+    PaymentId: Number(PaymentId),
+    Shops: [{ ShopCode: 481488, Amount: 3000, Fee: 210 }],
+  };
+  console.log({ newConfirmData });
+
+  const getStateEACQResultAfterInit = await getStateEACQ(tokenize(newConfirmData));
+  console.log({ getStateEACQResultAfterInit });
   
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await payInBrowser({
+    browser,
+    page,
+    url: PaymentURL,
+  });
+
+  const getStateEACQResultAfterPayInBrowser = await getStateEACQ(tokenize(newConfirmData));
+  console.log({ getStateEACQResultAfterPayInBrowser });
+
 };
 
 f();
